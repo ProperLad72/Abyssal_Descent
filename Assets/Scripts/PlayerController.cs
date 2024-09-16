6b034182ac6ac8f137d4bb0ff794e4c3dbe8b71c
ProperLad72 <nbell.personal@gmail.com>
2024-09-15 21:08:38 -0500

    Up to date code 9/15/2024

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 10.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            position.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            position.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            position.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            position.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = position;
+    }
+}
