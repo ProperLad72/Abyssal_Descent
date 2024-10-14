5763e33e9dc096f2cf79207018f359af8e4c8292
Cameron-Boddie <143846321+Cameron-Boddie@users.noreply.github.com>
2024-10-13 21:30:06 -0500

    Enemy w/ Follow Radius

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float moveSpeed;
+
+        public Rigidbody2D rb;
+
+        Vector2 movement;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MovementInput();
+        }
+
+        private void FixedUpdate()
+        {
+            rb.velocity = movement * moveSpeed;
+        }
+
+        void MovementInput()
+        {
+            float mx = Input.GetAxisRaw("Horizontal");
+            float my = Input.GetAxisRaw("Vertical");
+
+            movement = new Vector2(mx, my).normalized;
+        }
+    }
+}
