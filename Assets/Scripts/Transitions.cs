6490797f61d38efebeaa8d6149fa0bc0151ee7cb
ProperLad72 <nbell.personal@gmail.com>
2024-11-08 15:56:31 -0600

    trigger
    Trigger system implemented

---
+++ Assets/Scripts/Transitions.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transitions : MonoBehaviour
+{
+    [SerializeField] private string secondFloorSceneName = "Secondfloor";  // Ensure the correct scene name
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Start the scene transition coroutine
+            StartCoroutine(TransitionToNewScene());
+        }
+    }
+
+    private IEnumerator TransitionToNewScene()
+    {
+        if (string.IsNullOrEmpty(secondFloorSceneName)) 
+        {
+            Debug.LogError("Scene name is empty or null! Please set it in the Inspector.");
+        }
+        else
+        {
+            Debug.Log("Attempting to load scene: " + secondFloorSceneName);
+            yield return SceneManager.LoadSceneAsync(secondFloorSceneName);
+            SceneManager.UnloadSceneAsync("MainScene");
+        }
+    }
+}
